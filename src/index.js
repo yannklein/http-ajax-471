201449845fb1fba29384d892083da89582f2258a
Yann Klein <yann.klein@me.com>
2020-10-28 10:34:15 +0900

    lecture done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 // Rehearsal
 // //////////////////////
 
-// // 1.Select the button
+// 1. Select the button
 // const button = document.querySelector("#click-me");
-// // 2. Listen to a click
+
+// // 2. Listen to have click
 // button.addEventListener("click", (event) => {
-//   // 3. Change the DOM (add disabled class and change innerText to loading)
-//   // event.currentTarget.classList.add("disabled");
+//   // 3. Change the DOM, add the disable class + Loading...
+//   event.currentTarget.classList.add("disable");
 //   event.currentTarget.innerText = "Loading...";
 // });
 
@@ -15,17 +16,18 @@
 // HTTP GET request
 // //////////////////////
 
-// 1. Select elements
+// 1. Select the button, the input and the ul
+const button = document.querySelector("#submit");
 const input = document.querySelector("#keyword");
-const submit = document.querySelector("#submit");
 const results = document.querySelector("#results");
 
 const displayMovies = (movies) => {
+  results.innerHTML = "";
   movies.forEach((movie) => {
     results.insertAdjacentHTML(
       "beforeend",
       `<li class='list-inline-item'>
-        <img src="${movie.Poster}" alt="" />
+        <img src="${movie.Poster}" alt="poster">
         <p>${movie.Title}</p>
       </li>`
     );
@@ -37,34 +39,28 @@ const fetchMovies = (keyword) => {
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      results.innerHTML = "";
-      console.log(data);
-      // 3. Change the DOM
-      displayMovies(data.Search);
+      const movies = data.Search; // array of 10 movies
+      // 3. Display movies in the ul
+      displayMovies(movies);
     });
 };
 
 // 2. Listen to a click
-submit.addEventListener("click", (event) => {
+button.addEventListener("click", (event) => {
   event.preventDefault();
-  // 2.5. Fetch the API
-  const keyword = input.value;
-  fetchMovies(keyword);
+  // 2.5 Fetch an API
+  fetchMovies(input.value);
 });
-
 
 // //////////////////////
 // HTTP POST request
 // //////////////////////
 
 const searchAlgoliaPlaces = (event) => {
-  fetch(
-    "https://places-dsn.algolia.net/1/places/query",
-    {
-      method: "POST",
-      body: JSON.stringify({ query: event.currentTarget.value })
-    }
-  )
+  fetch("https://places-dsn.algolia.net/1/places/query", {
+    method: "POST",
+    body: JSON.stringify({ query: event.currentTarget.value })
+  })
     .then(response => response.json())
     .then((data) => {
       console.log(data.hits); // Look at local_names.default
@@ -73,3 +69,11 @@ const searchAlgoliaPlaces = (event) => {
 
 const search = document.querySelector("#search");
 search.addEventListener("keyup", searchAlgoliaPlaces);
+
+
+
+
+
+
+
+
